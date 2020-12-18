0bbe90f976b356256df07d0e038e0328a35c71a9
Dmitry Yevchuk <dmitry.yevchuk@gmail.com>
2020-12-18 18:54:01 +0200

    seventeenth homework

---
+++ Redux_books/books.js
@@ -0,0 +1,14 @@
+import {defaultBooks} from '../books'
+import C from '../constants'
+import {generate as id} from 'shortid'
+
+export default (state= defaultBooks, action) => {
+    const {type, payload} = action
+
+    switch(type) {
+        case C.ADD_BOOK: return [{_id: id(), ...payload}, ...state]
+        case C.EDIT_BOOK: return [payload, ...state.filter(book => book._id !== payload._id)]
+
+        default: return state
+    }
+}
